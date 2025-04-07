292accbf98d864cf78afdb41482885ba7f6cac49
raihan0504 <mhd.raihan0504@gmail.com>
2025-04-07 14:19:37 +0700

    Update IBookService.java

--- IBookService.java
+++ IBookService.java
@@ -1,1 +1,7 @@
-raihan
+// Interface Provided untuk layanan buku
+public interface IBookService {
+    boolean addBook(Book book);
+    boolean removeBook(String isbn);
+    Book findBookByIsbn(String isbn);
+    List<Book> searchBooksByTitle(String title);
+    List<Book> getAllBooks();
