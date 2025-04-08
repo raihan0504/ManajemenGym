fabd67c29af697e9f18192ca773cfa8d50c3a70a
aderiskimhs <165137730+aderiskimhs@users.noreply.github.com>
2025-04-09 00:41:08 +0700

    Create BorrowingRepository.java

---
+++ BorrowingRepository.java
@@ -0,0 +1,8 @@
+// Interface required untuk penyimpanan peminjaman
+interface BorrowingRepository {
+    void save(Borrowing borrowing);
+    Borrowing findByBookId(String bookId);
+    List<Borrowing> findByMemberId(String memberId);
+    void delete(String bookId, String memberId);
+}
+
