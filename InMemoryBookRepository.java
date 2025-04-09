ca2a63c9f9e6719081abea3e91fdb219844d6d5e
aderiskimhs <165137730+aderiskimhs@users.noreply.github.com>
2025-04-09 12:21:28 +0700

    update

---
+++ InMemoryBookRepository.java
@@ -0,0 +1,25 @@
+// Implementasi BookRepository
+class InMemoryBookRepository implements BookRepository {
+    private Map<String, Book> books = new HashMap<>();
+
+    @Override
+    public void save(Book book) {
+        books.put(book.getId(), book);
+    }
+
+    @Override
+    public Book findById(String id) {
+        return books.get(id);
+    }
+
+    @Override
+    public List<Book> findAll() {
+        return new ArrayList<>(books.values());
+    }
+
+    @Override
+    public boolean delete(String id) {
+        return books.remove(id) != null;
+    }
+}
+
