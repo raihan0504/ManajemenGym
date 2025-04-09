2379d13b953b53ceb005b6bace97b2bc03042cea
aderiskimhs <165137730+aderiskimhs@users.noreply.github.com>
2025-04-09 12:18:37 +0700

    comit

---
+++ BookRepository.java
@@ -0,0 +1,7 @@
+// Interface required untuk penyimpanan buku
+interface BookRepository {
+    void save(Book book);
+    Book findById(String id);
+    List<Book> findAll();
+    boolean delete(String id);
+}
